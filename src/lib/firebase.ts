0a9d06d0d712203990990ef57a2dda08bb44656e
masa08 <ushimasarug08@gmail.com>
2021-02-21 08:52:46 +0900

    :sparkles: Add firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,16 @@
+import firebaseApp from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurement_id: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+}
+
+firebaseApp.initializeApp(config);
+
+export default firebaseApp;
