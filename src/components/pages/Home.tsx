4d1d68c2bb2e85c23f36c1397d28238247731107
masa08 <ushimasarug08@gmail.com>
2021-02-20 22:35:45 +0900

    :sparkles: Add styled-components

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Home: React.FC = () => {
   return (
-    <h1>hello</h1>
+    <Head>hello</Head>
   )
 }
+
+const Head = styled.h1`
+  color: red;
+`
 
 export default Home;
